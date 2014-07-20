9847198a16fd1135caa2d47092e216bddf58df51
dustin <dushaobindoudou@gmail.com>
2014-07-20 18:56:11 +0800

    init file

---
+++ application/views/welcome_message.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>北京欢迎你</title>
+	<style type="text/css">
+
+	::selection{ background-color: #E13300; color: white; }
+	::moz-selection{ background-color: #E13300; color: white; }
+	::webkit-selection{ background-color: #E13300; color: white; }
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+	html,body{
+		width:100%;
+		height:100%;
+		overflow:hidden;
+	}
+	body{
+		font: 13px/20px normal 'Miscrsoft Yahei' Helvetica, Arial, sans-serif;
+		color: #FFF;
+		margin:0 0;
+	}
+	.bg{
+		width:100%;
+		height:100%;
+		overflow:hidden;
+	}
+	.bg div{
+		width:100%;
+		height:100%;
+		overflow:hidden;
+	}
+	.bg div table{
+		width:100%;
+		height:100%;
+		overflow:hidden;
+	}
+	.bg table img{
+		width:100%;
+		height:100%;
+		overflow:hidden;
+	}
+	h1 {
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+	h4{
+		font-weight: normal;
+	}
+	.content{
+		width:100%;
+		height:100%;
+		top:0;
+		left:0;
+		position:absolute;
+		color:#FFF;
+	}
+	.content .header{
+		width:700px;
+		margin:100px 0 0 50px;
+	}
+	.footer{
+		position:fixed;
+		bottom:0;
+		right:5px;
+		font-size:12px;
+	}
+	</style>
+</head>
+<body>
+	<div class="bg">
+		<div>
+			<table cellpadding="0" cellspacing="0">
+				<tr>
+					<td><img alt="" src="http://ww3.sinaimg.cn/large/8dd3f635jw1eigqxn6qbyj215o0rswr7.jpg" /></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="header">
+				<h1>北京欢迎你</h1>
+				<h4>
+				北京是中华人民共和国的首都、直辖市和国家中心城市，是全国的政治、文化、科教和国际交往中心，是世界著名的古都和现代国际城市，也是中华人民共和国中央人民政府和全国人民代表大会的办公所在地。
+		北京位于华北平原的北部，背靠燕山，有永定河流经老城西南，毗邻天津市和河北省。
+				</h4>
+			</div>
+		</div>
+		<div class="footer">design by dustin</div>
+	</div>
+	<div style="display:none;">
+	<script>
+		var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3a5e4bffb01cf8fc0ee294f8d5b555d0' type='text/javascript'%3E%3C/script%3E"));
+</script>
+	</div>
+</body>
+</html>
